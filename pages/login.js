066e1b9263145927308a09e6fc0d747ade00bbc9
sojib076 <sojibds332@gmail.com>
2022-12-03 12:13:03 +0600

    login

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,37 @@
-
-
+import React from 'react';
+import loginimg from './Assests/Authentication.gif'
 const login = () => {
     return (
-        <div>
-            
+        <div className="hero min-h-screen bg-white">
+            <div className="hero-content flex-col lg:flex-row-reverse w-[90%] shadow-2xl h-[95vh] ">
+                <div className=" w-[50%]">
+                    {/* <Image src={loginimg} alt="loginimg" className=''/>
+             */}
+                    <img src={loginimg} alt="" srcset="" />
+                </div>
+                <div className="card flex-shrink-0  max-w-sm shadow-2xl bg-base-100 w-[40%]">
+                    <div className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
